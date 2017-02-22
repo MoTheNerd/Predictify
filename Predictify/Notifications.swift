b61985902fa336589fdf8e0826e90cbf8f196000
Mohammad Al-Ahdal <mkaa00x@live.co.uk>
2017-02-22 02:51:28 -0700

    Did a lot of shit I just gotta feel proud of. It's now 2:51AM and I am a proud hacker on this solodude hackathon team tbh tbh tbh

---
+++ Predictify/Notifications.swift
@@ -0,0 +1,9 @@
+//
+//  Notifications.swift
+//  Predictify
+//
+//  Created by Mohammad Al-Ahdal on 2017-02-22.
+//  Copyright © 2017 Mohammad Al-Ahdal. All rights reserved.
+//
+
+import Foundation
